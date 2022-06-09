5bd1240ca2e89081871eb9415490a995a097ea88
GarciaRenan <renan.garcia@labellamafia.com.br>
2022-06-09 14:04:06 -0300

    fixing home button and added link to the faccao name in the description table - PCP

--- src/app/guard/login.guard.ts
+++ src/app/guard/login.guard.ts
@@ -23,9 +23,9 @@ export class LoginGuard implements CanActivate {
     | Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree> {
     if (this._userService.isLogged()) {
-      if (this._userService.getNivel() == 1 || this._userService.getNivel() == 99) {
+      if (this._userService.getNivel() == 1) {
         this._route.navigate(['auditor']);
-      } else if (this._userService.getNivel() == 2) {
+      } else if (this._userService.getNivel() == 2 || this._userService.getNivel() == 99) {
         this._route.navigate(['pcp']);
       }
       return false;
